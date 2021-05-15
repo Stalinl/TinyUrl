129968e916c84b78f9ca29ef6220c768675093ed
Stalin Lazar <stalin.lazar@experian.com>
2021-05-15 16:45:31 +0800

    Added webApi project

---
+++ src/WebApi/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TinyUrl.WebApi")]
+[assembly: AssemblyDescription("REST web service implementation for Tiny Url.")]
+[assembly: AssemblyVersionAttribute("1.0.0.0")]
+[assembly: AssemblyProduct("TinyUrl.WebApi")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f7e40380-85fb-4795-9f8f-a29d05c44ea6")]
+[assembly: NeutralResourcesLanguage("en-US")]
