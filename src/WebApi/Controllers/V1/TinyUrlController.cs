ed714d3f9f61f22daac344a9c6c0126d783004a6
Stalin Lazar <stalin.lazar@experian.com>
2021-05-15 18:29:42 +0800

    Added controller for web api

---
+++ src/WebApi/Controllers/V1/TinyUrlController.cs
@@ -0,0 +1,24 @@
+﻿namespace WebApi.Controllers.V1
+{
+    using System.Web.Http;
+
+    using EnsureThat;
+    using TinyUrl.Core;
+
+    [RoutePrefix("api/v1")]
+    public class TinyUrlController : ApiController
+    {
+        private readonly ITinyUrlService urlService;
+
+        public TinyUrlController(ITinyUrlService urlService)
+        {
+            this.urlService = EnsureArg.IsNotNull(urlService, nameof(urlService));
+        }
+
+        // GET
+        public IHttpActionResult Index()
+        {
+            return this.Ok();
+        }
+    }
+}
