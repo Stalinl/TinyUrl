126648ec642c72a48f3ca86a23b1c1471687d866
Stalin Lazar <stalin.lazar@experian.com>
2021-05-16 14:58:14 +0800

    Added Unit test project for web app

---
+++ src/Web/Controllers/TinyUrlController.cs
@@ -0,0 +1,14 @@
+﻿namespace TinyUrl.Web.Controllers
+{
+    using System;
+    using System.Web.Mvc;
+
+    public class TinyUrlController : Controller
+    {
+        // GET: TinyUrl
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
